b65460c29c2d6b5f2d862fe8379c24c061c9cdf6
Luis Tejeda <luis.tejeda@contpaqi.com>
2019-03-20 16:26:27 -0600

    Carga de proyecto

---
+++ src/TenXamarinD5/TenXamarinD5/TenXamarinD5.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+
+namespace TenXamarinD5.Droid
+{
+    [Activity(Label = "TenXamarinD5", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            string fileName = "database.db3";
+            string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string fullPath = Path.Combine(folderPath, fileName); //Added using System.IO;
+
+
+            LoadApplication(new App(fullPath));
+        }
+    }
+}
---
+++ src/TenXamarinD5/TenXamarinD5/TenXamarinD5/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TenXamarinD3.Model;
+using Xamarin.Forms;
+
+namespace TenXamarinD5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckIfShouldBeEnabled()
+        {
+            saveButton.IsEnabled = false;
+            if (!string.IsNullOrWhiteSpace(titleEntry.Text) && !string.IsNullOrWhiteSpace(contentEditor.Text))
+                saveButton.IsEnabled = true;
+        }
+
+        private void TitleEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckIfShouldBeEnabled();
+        }
+
+        private void ContentEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckIfShouldBeEnabled();
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+
+            Experience newExperience = new Experience()
+            {
+                Title = titleEntry.Text,
+                Content = contentEditor.Text,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+            var inserted = 0;
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+            {
+                conn.CreateTable<Experience>();
+                inserted = conn.Insert(newExperience);
+            }
+
+            if (inserted > 0)
+            {
+                titleEntry.Text = string.Empty;
+                contentEditor.Text = string.Empty;
+            }
+            else
+            {
+                DisplayAlert("Error", "There was an error inserting the experience", "ok");
+            }
+            titleEntry.Text = string.Empty;
+            contentEditor.Text = string.Empty;
+
+        }
+
+        private void CancelButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
